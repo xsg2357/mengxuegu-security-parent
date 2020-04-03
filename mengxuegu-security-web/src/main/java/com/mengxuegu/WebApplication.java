a3a0207fc30bf858e14c2eb2780df55573f6e21d
xiang <1069342827@qq.com>
2020-04-03 22:03:34 +0800

    net username alter

--- mengxuegu-security-web/src/main/java/com/mengxuegu/WebApplication.java
+++ mengxuegu-security-web/src/main/java/com/mengxuegu/WebApplication.java
@@ -3,6 +3,7 @@ package com.mengxuegu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@PreFilter和@PostFilter可以对集合类型的参数或返回值进行过滤
 @SpringBootApplication
 public class WebApplication {
 
