05c900363e3990eb0ba6f115612341c41dcb7434
xiang <1069342827@qq.com>
2020-04-03 21:13:29 +0800

    user

--- mengxuegu-security-web/src/main/java/com/mengxuegu/web/mapper/SysUserMapper.java
+++ mengxuegu-security-web/src/main/java/com/mengxuegu/web/mapper/SysUserMapper.java
@@ -1,8 +1,14 @@
 package com.mengxuegu.web.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mengxuegu.web.entities.SysUser;
+import org.apache.ibatis.annotations.Param;
 
 public interface SysUserMapper extends BaseMapper<SysUser> {
+
+
+    IPage<SysUser> selectPage(Page<SysUser> page, @Param("u") SysUser sysUser);
 
 }
